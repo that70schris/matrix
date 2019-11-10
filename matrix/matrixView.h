7bd37afb34cc538779c890700dd5b1b852a07adf
Chris Bailey <chris@bailey.codes>
2019-11-10 14:00:56 -0800

    Initial Commit

---
+++ matrix/matrixView.h
@@ -0,0 +1,13 @@
+//
+//  matrixView.h
+//  matrix
+//
+//  Created by Chris Bailey on 11/10/19.
+//  Copyright © 2019 baileyCode. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface matrixView : ScreenSaverView
+
+@end
