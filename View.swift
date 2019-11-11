787fa8841202b231c8430190df7f2a31334c84f0
Chris Bailey <chris@bailey.codes>
2019-11-10 23:12:30 -0800

    animation

--- View.swift
+++ View.swift
@@ -1,15 +1,37 @@
 import ScreenSaver
 
 class Matrix: ScreenSaverView {
-
-  override func draw(_ dirtyRect: NSRect) {
-    super.draw(dirtyRect)
-
-    let hello: NSString = "hello SWIFT screen saver plugin"
-    hello.draw(at: .zero, withAttributes: [
-      .foregroundColor: NSColor.green,
-      .font: NSFont(name: "Monaco", size: 14) ?? NSFont.monospacedSystemFont(ofSize: 14, weight: .medium),
-    ])
+  var j: Int = 0
+  let font = NSFont(name: "Monaco", size: 44) ?? NSFont.monospacedSystemFont(ofSize: 14, weight: .medium)
+  
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
   }
   
+  override init?(frame: NSRect, isPreview: Bool) {
+    super.init(frame: frame, isPreview: isPreview)
+    animationTimeInterval = 1
+  }
+  
+  override func animateOneFrame() {
+    needsDisplay = true
+  }
+  
+  override func draw(_ rect: NSRect) {
+    NSAttributedString(string: "\(self.j)",
+      attributes: [
+        .backgroundColor: NSColor.red,
+        .foregroundColor: NSColor.green,
+        .font: self.font,
+    ]).draw(at: NSPoint(x: 0, y: self.j * Int(self.font.pointSize)))
+    self.j += 1
+  }
+  
+  override var isFlipped: Bool {
+    return true
+  }
+  
+  override var hasConfigureSheet: Bool {
+    return true
+  }
 }
